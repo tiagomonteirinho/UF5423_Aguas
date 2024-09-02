09ef21055b5a937f1e3657a1d8ebc9a461b113fc
tiagomonteirinho <tiagoamonteirinho@gmail.com>
2024-09-02 19:37:26 +0100

    Added project files.

---
+++ UF5423_Aguas/UF5423_Aguas/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace UF5423_Aguas.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
